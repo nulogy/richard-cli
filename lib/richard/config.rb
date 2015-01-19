598782d2f14a50c0b1628f0205dc57cfebe400ef
Adam Kerr <adamk@nulogy.com>
2015-01-19 09:08:27 -0500

    Fixes default url

--- lib/richard/config.rb
+++ lib/richard/config.rb
@@ -2,8 +2,7 @@ module Richard
   class Config
     attr_reader :api_key, :url
 
-    # DEFAULT_URL = "http://richard.nulogy.nu/"
-    DEFAULT_URL = 'http://richard.vcap.me:3030'
+    DEFAULT_URL = "http://richard.nulogy.nu"
     CONFIG_FILE = ENV['HOME'] + "/.richard"
 
     def initialize(url, api_key)
